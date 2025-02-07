51d984b4571beb5fe277c8277e0b574c057bff45
bochaozhang <bochao.zhang@my.jcu.edu.au>
2025-02-07 18:38:08 +0800

    practical 2 password_stars.py

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,21 @@
-length_required = 10
-password = input("please enter your password: ")
-if len(password) < length_required:
-    print("password is too short")
+def main():
+    length_required = 10
+    password = get_password()
+    if len(password) < length_required:
+        print("password is too short")
+        password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    """This function will print asterisks of length length"""
+    print(len(password) * "*")
+
+
+def get_password():
+    """This function will let the user enter a password"""
     password = input("please enter your password: ")
-print(len(password)*"*")
+    return password
+
+
+main()
