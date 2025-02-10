419cee02208c4a85d3f29927fc5bbe67b3349606
bochaozhang <bochao.zhang@my.jcu.edu.au>
2025-02-10 09:33:31 +0800

    practical 2 finished

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,19 @@
 """Module 2 - practical 2 """
 
 def main():
+    """This is the main function and it will check the password and print stars"""
     length_required = 10
-    password = get_password()
+    password = get_password(length_required)
+    print_asterisks(password)
+
+
+def get_password(length_required):
+    """this function will get the password."""
+    password = input("please enter your password: ")
     while len(password) < length_required:
         print("password is too short")
         password = get_password()
-    print_asterisks(password)
+    return password
 
 
 def print_asterisks(password):
@@ -14,9 +21,6 @@ def print_asterisks(password):
     print(len(password) * "*")
 
 
-def get_password():
-    """This function will let the user enter a password"""
-    password = input("please enter your password: ")
     return password
 
 
