0e0f48427caa3c3974322a9719b6197991780419
bochaozhang <bochao.zhang@my.jcu.edu.au>
2025-02-16 09:39:15 +0800

    practical 3 complete version

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+"""
+the smallest number is 5 and the largest is 20
+"""
+print(random.randrange(3, 10, 2))  # line 2
+"""
+the smallest is 3 and the largest is 9
+"""
+print(random.uniform(2.5, 5.5))  # line 3
+"""
+The smallest is 2.5, biggest is 5.5 
+"""
+print(random.uniform(1,100))
