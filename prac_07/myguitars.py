997030f66db040323243ed7a0e0bd9365278e6cd
bochaozhang <bochao.zhang@my.jcu.edu.au>
2025-03-23 10:47:07 +0800

    complete

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -9,6 +9,35 @@ def main():
     guitarscsv.sort()
     print_pricise_guitar_infor(guitarscsv)
 
+    added_guitars = add_new_guitars()
+    if added_guitars:
+        guitarscsv.extend(added_guitars)
+        guitarscsv.sort()
+        print_pricise_guitar_infor(guitarscsv)
+    else:
+        print("no guitars added")
+
+
+    save_file(guitarscsv)
+
+
+def save_file(guitarscsv):
+    """This funciton will save the guitar back to csv file"""
+    with open(FILENAME, 'w') as out_file:
+        for guitar in guitarscsv:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+
+
+def add_new_guitars():
+    """This function will ask the users to input the guitars and add it into the list"""
+    appeneded_guitars = []
+    name = input("Type in your guitar's name: ")
+    while name != "":
+        year = int(input("year:"))
+        price = float(input("whats the price? "))
+        appeneded_guitars.append(Guitar(name, year, price))
+        name = input("Type in your guitar's name")
+    return appeneded_guitars
 
 def load_guitarcsv():
     """This function will load the csv file and store it"""
